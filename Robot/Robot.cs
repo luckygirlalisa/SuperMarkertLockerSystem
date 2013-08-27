6f5dc6f13e7dd51b39fc3e43bb91b55ed63c1a58
unknown <zfxiao@zfxiao.corporate.thoughtworks.com>
2013-08-27 23:18:58 +0800

    robot can store and pick multiple bags

--- Robot/Robot.cs
+++ Robot/Robot.cs
@@ -2,15 +2,43 @@
 {
     public class Robot
     {
+        private int capacity;
+        private Locker[] lockers;
         private Locker locker = new Locker();
+        private int index = 0;
+
+        public Robot(int capacity)
+        {
+            this.capacity = capacity;
+            lockers = new Locker[this.capacity];
+            for (int i = 0; i < capacity; i++)
+            {
+                lockers[i] = new Locker();
+            }
+        }
+
         public Ticket Store(Bag bag)
         {
-            return locker.Store(bag);
+            if (index > capacity-1)
+            {
+                return null;
+            }
+            Ticket ticket = lockers[index].Store(bag);
+            index++;
+            return ticket;
         }
 
         public Bag Pick(Ticket ticket)
         {
-            return locker.Pick(ticket);
+            for (int i = 0; i < capacity; i++)
+            {
+                Bag bag = lockers[i].Pick(ticket);
+                if (bag != null)
+                {
+                    return bag;
+                }
+            }
+            return null;
         }
     }
 }
--- RobotTest/RobotTest.cs
+++ RobotTest/RobotTest.cs
@@ -5,10 +5,12 @@ namespace SuperMarketLockerSystem
     class RobotTest
     {
         private Robot robot;
+        private int capacity;
         [SetUp]
         public void SetUp()
         {
-            robot = new Robot();
+            capacity = 10;
+            robot = new Robot(capacity);
         }
 
         [Test]
@@ -21,16 +23,6 @@ namespace SuperMarketLockerSystem
             //then
             Assert.NotNull(ticket);
         }
-        [Test]
-        public void should_return_null_when_store_in_stored_locker()
-        {
-            //given
-            var bag = new Bag();
-            //when
-            robot.Store(bag);
-            //then
-            Assert.Null(robot.Store(new Bag()));
-        }
 
         [Test]
         public void should_return_the_same_bag_stored_when_pick_with_a_ticket()
@@ -39,6 +31,35 @@ namespace SuperMarketLockerSystem
             var ticket = robot.Store(storedBag);
             Bag pickedBag = robot.Pick(ticket);
             Assert.That(pickedBag, Is.SameAs(storedBag));
+        }
+
+        [Test]
+        public void should_be_able_to_store_in_multiple_locker()
+        {
+            var bag1 = new Bag();
+            var bag2 = new Bag();
+            robot.Store(bag1);
+            Assert.NotNull(robot.Store(bag2));
+        }
+
+        [Test]
+        public void should_return_null_when_store_with_all_lockers_are_stored()
+        {
+            for (int i = 0; i < capacity; i++)
+            {
+                robot.Store(new Bag());
+            }
+            Assert.Null(robot.Store(new Bag()));
+        }
+
+        [Test]
+        public void should_return_correct_bag_when_pick_with_ticket_after_multiple_store()
+        {
+            var bag1 = new Bag();
+            var bag2 = new Bag();
+            robot.Store(bag1);
+            var ticket2 = robot.Store(bag2);
+            Assert.That(bag2, Is.SameAs(robot.Pick(ticket2)));
         }
     }
 }
