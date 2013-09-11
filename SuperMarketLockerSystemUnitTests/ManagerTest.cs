6b747261f360de92f272b5d8ac27aa18e5c24999
zfxiao <zfxiao@thoughtworks.com>
2013-09-11 20:40:39 +0800

    store bag into robot when locker is full

--- SuperMarketLockerSystemUnitTests/ManagerTest.cs
+++ SuperMarketLockerSystemUnitTests/ManagerTest.cs
@@ -7,27 +7,55 @@ namespace SuperMarketLockerSystemUnitTests
     public class ManagerTest
     {
         [Test]
-        public void Should_return_ticket_when_store_bag_into_locker()
+        public void Should_store_into_locker_when_locker_is_not_full()
         {
             var locker = new Locker(2);
             var manager = new Manager(locker);
+            
             var ticket = manager.Store(new Bag());
+            
             Assert.NotNull(ticket);
             Assert.NotNull(locker.Pick(ticket));
         }
+
+        [Test]
+        public void Should_store_into_robot_when_locker_is_full()
+        {
+            var lockerForRobot = new Locker(1);
+            var robot = new Robot();
+            robot.Manage(new List<Locker>{lockerForRobot});
+            var lockerForManager = new Locker(1);
+            var manager = new Manager(lockerForManager,robot);
+            
+            manager.Store(new Bag());
+            var ticket = manager.Store(new Bag());
+
+            Assert.NotNull(ticket);
+            Assert.NotNull(robot.Pick(ticket));
+        }
+
     }
 
     public class Manager
     {
-        private Locker locker;
+        private readonly Locker locker;
+        private Robot robot;
+
         public Manager(Locker locker)
         {
             this.locker = locker;
         }
 
+        public Manager(Locker locker, Robot robot)
+        {
+            this.locker = locker;
+            this.robot = robot;
+        }
+
         public Ticket Store(Bag bag)
         {
-            return locker.Store(bag);
+            var ticket = locker.Store(bag);
+            return ticket ?? robot.Store(bag);
         }
     }
 }
