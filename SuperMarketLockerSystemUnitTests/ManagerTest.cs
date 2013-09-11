dc459fdf45e0d278a64b03d37b6f18b19607dd5d
zfxiao <zfxiao@thoughtworks.com>
2013-09-11 20:58:38 +0800

    should store into smart robot when locker and robot are both full

--- SuperMarketLockerSystemUnitTests/ManagerTest.cs
+++ SuperMarketLockerSystemUnitTests/ManagerTest.cs
@@ -34,28 +34,42 @@ namespace SuperMarketLockerSystemUnitTests
             Assert.NotNull(robot.Pick(ticket));
         }
 
+        [Test]
+        public void Should_store_into_smart_robot_when_locker_and_robot_are_full()
+        {
+            var smartRobot = new SmartRobot();
+            smartRobot.Manage(new List<Locker>{new Locker(1)});
+
+            var robot = new Robot();
+            robot.Manage(new List<Locker>{new Locker(1)});
+
+            var manager = new Manager(new Locker(1), robot, smartRobot);
+           
+            manager.Store(new Bag());
+            manager.Store(new Bag());
+            var ticket = manager.Store(new Bag());
+            Assert.NotNull(ticket);
+            Assert.NotNull(smartRobot.Pick(ticket));
+        }
     }
 
     public class Manager
     {
         private readonly Locker locker;
-        private Robot robot;
+        private readonly Robot robot;
+        private readonly SmartRobot smartRobot;
 
-        public Manager(Locker locker)
-        {
-            this.locker = locker;
-        }
-
-        public Manager(Locker locker, Robot robot)
+        public Manager(Locker locker, Robot robot = null, SmartRobot smartRobot = null)
         {
             this.locker = locker;
             this.robot = robot;
+            this.smartRobot = smartRobot;
         }
 
         public Ticket Store(Bag bag)
         {
-            var ticket = locker.Store(bag);
-            return ticket ?? robot.Store(bag);
+            var ticket = locker.Store(bag) ?? (robot.Store(bag) ?? smartRobot.Store(bag));
+            return ticket;
         }
     }
 }
