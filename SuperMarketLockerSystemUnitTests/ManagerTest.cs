83aa9d22ceb537f887db8c8d73c01375b7e95245
zfxiao <zfxiao@thoughtworks.com>
2013-09-11 19:54:37 +0800

    [Chenghong/Zhanfei] Add manager and can store into locker.

---
+++ SuperMarketLockerSystemUnitTests/ManagerTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SuperMarketLockerSystem;
+
+namespace SuperMarketLockerSystemUnitTests
+{
+    public class ManagerTest
+    {
+        [Test]
+        public void Should_return_ticket_when_store_bag_into_locker()
+        {
+            var locker = new Locker(2);
+            var manager = new Manager(locker);
+            var ticket = manager.Store(new Bag());
+            Assert.NotNull(ticket);
+            Assert.NotNull(locker.Pick(ticket));
+        }
+    }
+
+    public class Manager
+    {
+        private Locker locker;
+        public Manager(Locker locker)
+        {
+            this.locker = locker;
+        }
+
+        public Ticket Store(Bag bag)
+        {
+            return locker.Store(bag);
+        }
+    }
+}
